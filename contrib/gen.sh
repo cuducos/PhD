b0eb5a8fc4047bce9de087135e4164991112831f
Eduardo Cuducos <cuducos@gmail.com>
2016-05-27 11:14:13 -0300

    Specify stylesheet inside AsciiDoctor files

--- contrib/gen.sh
+++ contrib/gen.sh
@@ -1,14 +1,13 @@
-echo "AsciiDoctor document: $2.adoc"
-echo "Path to the CSS is: $1 (relative to the AsciiDoctor document)"
+echo "AsciiDoctor document: $1.adoc"
 
 echo "\n==> Generating HTML"
-asciidoctor -a stylesheet=$1 $2.adoc
+asciidoctor $1.adoc
 
 echo "\n==> Removing footnote square brackets"
-python contrib/bracketless.py $2.html
+python contrib/bracketless.py $1.html
 
 echo "\n==> Generating PDF"
-weasyprint $2.html $2.pdf
+weasyprint $1.html $1.pdf
 
 echo "\n==> Generating DOCX"
-pandoc -o $2.docx $2.html
+pandoc -o $1.docx $1.html
